074d0c78784716213cd9be2486df50d86d1d2d87
Guilherme Miranda <saguilherme16@gmail.com>
2025-09-01 21:50:14 -0300

    DOCS: add comment explaining the head function in selectFirstRows

--- LeetCode2879.py
+++ LeetCode2879.py
@@ -3,3 +3,4 @@ import pandas as pd
 def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
     return employees.head(3)
 
+#  head serve para mostrar oq tem dentro do data frame(tipo um output), e quando colocamos um numero dentro dele limitamos a quantidade de dados q vai aparecer nesse caso das rows ou colunas
