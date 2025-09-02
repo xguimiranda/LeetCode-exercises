4eb81d117c6fd1c67a864635c3c737ea2dfbb1bb
Guilherme Miranda <saguilherme16@gmail.com>
2025-09-01 21:35:53 -0300

    DOCS: add detailed docstring for getDataframeSize function

--- LeetCode2878.py
+++ LeetCode2878.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def getDataframeSize(players: pd.DataFrame) -> List[int]:
     return [players.shape[0], players.shape[1]]
-# 
+# Every DataFrame in pandas has a shape attribute. When you call it, it returns a tuple (number of rows, number of columns). In our case, for the given players DataFrame, the shape would be (10, 5) because there are 10 players and 5 attributes for each player.
 
 def getDataframeSizeV2(players: pd.DataFrame) -> List[int]:
     return list(players.shape)
