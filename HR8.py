6201ead873bf5a8004d3ec1417cb07563e73a6ac
Guilherme Miranda <saguilherme16@gmail.com>
2025-09-07 20:43:14 -0300

    REFAC: remove additional commented-out code in HR8.py

--- HR8.py
+++ HR8.py
@@ -21,3 +21,10 @@ if __name__ == '__main__':
 #     arr = Counter(map(int, raw_input().split())).keys()
 #     arr.sort()
 #     print arr[-2]
+
+# from collections import Counter
+# if __name__ == '__main__':
+#     n = int(raw_input())
+#     arr = list(set(map(int, raw_input().split())))
+#     arr.sort()
+#     print arr[-2]
