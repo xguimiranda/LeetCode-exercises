61816caba21582146541801830e09952495607eb
Guilherme Miranda <saguilherme16@gmail.com>
2025-09-07 20:42:29 -0300

    REFAC: clean up commented-out code in HR8.py

--- HR8.py
+++ HR8.py
@@ -4,3 +4,13 @@ if __name__ == '__main__':
     scores = set(arr)
     scores.remove(max(scores))
     print(max(scores))
+
+# if __name__ == '__main__':
+#     n = int(raw_input())
+#     arr = map(int, raw_input().split())
+#     m1 = max(arr)
+#     m2 = -9999999999
+#     for i in range(n):
+#         if arr[i] != m1 and arr[i] > m2:
+#             m2 = arr[i]
+#     print m2
